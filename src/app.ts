d80293259ad5d573be16beea93545766a6235268
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-02-25 12:45:59 +0100

    finished my Finance Logger App

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,32 @@
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/payment.js";
 import { HasFormater } from "./interfaces/HasFormater.js";
-import { ListTemplate } from './classes/listTemplate.js';
+import { ListTemplate } from "./classes/listTemplate.js";
 
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 // console.log(form.children);
 
-const type = document.querySelector('#type') as HTMLSelectElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
 // list template instance
-const ul = document.querySelector('ul')!;
+const ul = document.querySelector("ul")!;
 const list = new ListTemplate(ul);
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
+form.addEventListener("submit", (e: Event) => {
+	e.preventDefault();
 
-    let doc: HasFormater;
-    if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-    } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-    }
+	let values: [string, string, number];
+	values = [tofrom.value, details.value, amount.valueAsNumber];
 
-    list.render(doc, type.value, "end");
+	let doc: HasFormater;
+	if (type.value === "invoice") {
+		doc = new Invoice(...values);
+	} else {
+		doc = new Payment(...values);
+	}
+
+	list.render(doc, type.value, "end");
 });
